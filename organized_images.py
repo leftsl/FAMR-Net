a0b956fee375b4cd699eb391eb5cc3c82a7a5799
leftsl <48373656+leftsl@users.noreply.github.com>
2025-03-07 15:30:18 +0800

    Update organized_images.py
    This is a classified file. After downloading the data, you need to run this file first.

--- organized_images.py
+++ organized_images.py
@@ -3,40 +3,42 @@ import pandas as pd
 import os
 from tqdm import tqdm
 import time
-# 读取CSV文件，这里假设CSV文件是逗号分隔的
-metadatafile = 'EN\HAM10000_metadata'  # 请确保文件名和路径正确
-df = pd.read_csv(metadatafile)
 
-# 图片原始路径和目标路径
-originalpath = 'EN\HAM10000\images'
-targetpath = 'EN\HAM10000\organizedimages'
+# Read the CSV file, assuming the CSV file is comma-separated
+metadata_file = 'IU_XRAY_metadata.csv'  # Make sure the filename and path are correct
+df = pd.read_csv(metadata_file)
 
-# 确保目标路径存在，如果不存在，则创建它
-if not os.path.exists(targetpath):
-    os.makedirs(targetpath)
+# Original and target paths for the images
+original_path = 'IU_XRAY/images'
+target_path = 'IU_XRAY/organized_images'
 
-# 遍历CSV文件中的每一行
-for index, row in tqdm(df.iterrows(),total=df.shape[0]):
-    # 获取图片名称和标签
-    imagename = row['image_id']  # 假设CSV文件中有一个名为'imageid'的列
-    label = row['dx']  # 假设CSV文件中有一个名为'dx'的列
-    # 构建原始图片的完整路径
-    originalimagepath = os.path.join(originalpath, imagename+'.jpg')
+# Ensure the target path exists, if not, create it
+if not os.path.exists(target_path):
+    os.makedirs(target_path)
+
+# Iterate over each row in the CSV file
+for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+    # Get the image name and label
+    image_name = row['ImageID']  # Assuming the CSV file has a column named 'ImageID'
+    label = row['Finding Label']  # Assuming the CSV file has a column named 'Finding Label'
     
-    # 检查文件是否存在，如果不存在，则跳过
-    if not os.path.exists(originalimagepath):
-        print(f"不存在的文件: {originalimagepath}")
+    # Build the full path for the original image
+    original_image_path = os.path.join(original_path, image_name + '.png')
+    
+    # Check if the file exists, if not, skip
+    if not os.path.exists(original_image_path):
+        print(f"File does not exist: {original_image_path}")
         continue
     
-    # 确保标签文件夹在目标路径中存在，如果不存在，则创建它
-    labelfolder = os.path.join(targetpath, label)
-    if not os.path.exists(labelfolder):
-        os.makedirs(labelfolder)
+    # Ensure the label folder exists in the target path, if not, create it
+    label_folder = os.path.join(target_path, label)
+    if not os.path.exists(label_folder):
+        os.makedirs(label_folder)
     
-    # 构建目标图片的完整路径
-    targetimagepath = os.path.join(labelfolder, imagename+'.jpg')
+    # Build the full path for the target image
+    target_image_path = os.path.join(label_folder, image_name + '.png')
     
-    # 移动图片
-    shutil.copy(originalimagepath, targetimagepath)
+    # Move the image
+    shutil.copy(original_image_path, target_image_path)
 
-print("图片分类完成。")
+print("Image classification completed.")
